03b775f1f65dcb0ac6193c2579fd9c5c4aa25772
Jazz <winsten.chou@gmail.com>
2021-06-05 18:46:03 +0800

    add unit test

--- src/Implement/UnitTestOperateData.cpp
+++ src/Implement/UnitTestOperateData.cpp
@@ -6,3 +6,31 @@
 #include <gtest/gtest.h>
 
 #include "OperateData.h"
+
+TEST(OPERATEDATA_TEST, Basic) {
+  OperateData test;
+  test.header.insert("method", "POST");
+  test.header.insert("url", "explorer_file");
+  test.header.insert("Host", "echo.paw.cloud");
+  test.header.insert("Content-Type", "application/json");
+  test.header.insert("User-Agent",
+                     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) "
+                     "Gecko/20100101 Firefox/53.0");
+  test.header.insert("Content-Length", "136");
+  test.body.insert("path", "test/myFile/filename.txt");
+  test.body.insert("pos", "0");
+  test.body.insert("data", "123456789");
+  OperateData test2 = test;
+  EXPECT_EQ("POST", test2.header["method"]);
+  EXPECT_EQ("explorer_file", test2.header["url"]);
+  EXPECT_EQ("echo.paw.cloud", test2.header["Host"]);
+  EXPECT_EQ("application/json", test2.header["Content-Type"]);
+  EXPECT_EQ(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:53.0) Gecko/20100101 "
+      "Firefox/53.0",
+      test2.header["User-Agent"]);
+  EXPECT_EQ("136", test2.header["Content-Length"]);
+  EXPECT_EQ("test/myFile/filename.txt", test2["path"]);
+  EXPECT_EQ("0", test2["pos"]);
+  EXPECT_EQ("123456789", test2["data"]);
+}
